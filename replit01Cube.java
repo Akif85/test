ee009d3b76f706d608f05cfd4a52054d545f59b6
Akif Rençber <98591869+Akif85@users.noreply.github.com>
2022-04-24 09:36:56 +0300

    Add files via upload

---
+++ replit01Cube.java
@@ -0,0 +1,17 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class replit01Cube {
+    public static void main(String[] args) {
+
+        Scanner scan =new Scanner (System.in);
+        System.out.println ("Lütfen bir sayı giriniz");
+        int num=scan.nextInt();
+        int cube=0;
+
+        for (int i = 0; i <cube; i++) {
+            System.out.println(i);
+        }
+    }
+}
